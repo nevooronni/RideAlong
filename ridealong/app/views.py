4a1bcd4c08420cda855a1b7fe147a412b4ecf955
Neville Oronni <nevooronni@gmail.com>
2017-12-03 22:47:03 +0300

    added first template welcome template

--- ridealong/app/views.py
+++ ridealong/app/views.py
@@ -1,5 +1,4 @@
-from django.http  import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+    return render(request, 'welcome.html')

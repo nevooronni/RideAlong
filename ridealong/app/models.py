afcd7afeed0e5e340342e94c095472ec86213249
Neville Oronni <nevooronni@gmail.com>
2017-12-05 15:08:38 +0300

    added model for rider and created table in the db

--- ridealong/app/models.py
+++ ridealong/app/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+
+class Rider(models.Model):
+	first_name = models.CharField(max_length = 30)
+	last_name = models.CharField(max_length = 30)
+	email = models.EmailField()
+	phone = models.IntegerField()
+	city = models.CharField(max_length = 30)
+
+	@classmethod
+	def rider_list(cls):
+		riders = Rider.objects.all()
+		return riders
+
+
 class Driver(models.Model):
 	first_name = models.CharField(max_length = 30)
 	last_name = models.CharField(max_length = 30)
@@ -14,3 +28,4 @@ class Driver(models.Model):
 	def drivers_list(cls):
 		drivers = Driver.objects.all()
 		return drivers
+

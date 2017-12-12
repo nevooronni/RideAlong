e9083e84673c5ad5f2cec4e15f321f260b86d109
Neville Oronni <nevooronni@gmail.com>
2017-12-12 15:50:05 +0300

    configured application to use images and added models to our admin page

--- ridealong/app/admin.py
+++ ridealong/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Driver,DriverProfile,DriverReview
 
-# Register your models here.
+admin.site.register(Driver)
+admin.site.register(DriverProfile)
+admin.site.register(DriverReview)
+
--- ridealong/app/urls.py
+++ ridealong/app/urls.py
@@ -21,4 +21,4 @@ if settings.DEBUG == True:
 	urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
-	urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_URL)
+	urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)

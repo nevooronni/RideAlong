8f9bf522139a6c62d2d529bd21e57dded10454d2
Neville Oronni <nevooronni@gmail.com>
2017-12-13 09:40:09 +0300

    initial commit

--- ridealong/settings.py
+++ ridealong/settings.py
@@ -126,5 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
